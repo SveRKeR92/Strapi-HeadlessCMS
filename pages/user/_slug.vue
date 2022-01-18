ce19ff051cee0ad5c748fdd04042406f89a11157
Maxime <maxime.attala@edu.devinci.fr>
2022-01-18 16:42:20 +0100

    init nuxt app

---
+++ pages/user/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+      <Tutorial />      
+</template>
+
+<script>
+export default {
+      name: "IndexPage",
+      middleware: ["auth", "middleware2"],
+      async asyncData() {
+            // console.log(this.$route.params);
+            await new Promise( (r) => {
+                  setTimeout(r, 5000);
+            })
+
+            return{}
+      },
+      
+};
+</script>
